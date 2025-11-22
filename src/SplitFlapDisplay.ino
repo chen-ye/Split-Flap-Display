8a2678c089245440ffc97765c1eda3fcaa90a0b3
Chen Ye <cye@reliable.co>
2025-11-22 09:55:58 -0800

    cye-personal

--- src/SplitFlapDisplay.ino
+++ src/SplitFlapDisplay.ino
@@ -42,7 +42,7 @@ JsonSettings settings = JsonSettings("config", {
     {"stepsPerRot", JsonSetting(2048)},
     {"maxVel", JsonSetting(15.0f)},
     {"charset", JsonSetting(48)},
-    {"custom_charset", JsonSetting(" ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789':?!.-/@$#%")},
+    {"custom_charset", JsonSetting(" ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789':/?!.->$#%")},
     // Operational States
     {"mode", JsonSetting(0)}
 });
@@ -87,6 +87,7 @@ void setup() {
         splitflapMqtt.setDisplay(&display);
         display.setMqtt(&splitflapMqtt);
         display.writeString("Hello");
+        display.writeString("Alex");
         delay(2500);
         display.writeString("");
     } else {
